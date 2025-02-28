67f40c00de5c161eb2976478f258e907c59ffe51
Natakula2006 <natakulaharshini@gmail.com>
2025-02-28 11:41:46 +0530

    Update svecw chatbot.py

--- svecw chatbot.py
+++ svecw chatbot.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import google.generativeai as genai
 from sklearn.metrics.pairwise import cosine_similarity
-form sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 st.set_page_config(page_title="Svecw college chatbot",layout="centered")
 if "messages" not in st.session_state:
   st.session_state.messages = []
